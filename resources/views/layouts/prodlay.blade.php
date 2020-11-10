47d6f4678e40370fd0ee68bc9104219fbb1169bc
Roberto Barros <robertobarrosx@gmail.com>
2020-11-10 17:29:01 -0300

    Update prodlay.blade.php

--- resources/views/layouts/prodlay.blade.php
+++ resources/views/layouts/prodlay.blade.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>App Name - @yield('title')</title>
+    <title>Compre Mais @yield('title')</title>
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
         rel="stylesheet">
